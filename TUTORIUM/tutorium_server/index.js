9239a7ccab3e79eaa95c850e98685fc3fa60d40c
ahmetyanik <ahmetyank4242@gmail.com>
2022-02-26 00:02:33 +0100

    Some apis created

--- TUTORIUM/tutorium_server/index.js
+++ TUTORIUM/tutorium_server/index.js
@@ -4,26 +4,142 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const res = require("express/lib/response");
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "*",
+    credentials: true,
+  })
+);
 
 const uri = process.env.BAGLANTI;
 
 const Schema = mongoose.Schema;
 
-mongoose.connect(uri,()=>{
-
-    console.log("Connect to DB");
+mongoose.connect(uri, () => {
+  console.log("Connect to DB");
 });
 
-app.get("/", (req,res)=>{
-    res.send("Baglanti basarili...");
-})
+app.get("/", (req, res) => {
+  res.send("Baglanti basarili...");
+});
 
+////////////////MONGODB SEMALARI//////
 
+const urunSema = {
+  isim: String,
+  kategori: String,
+  kategori_url: String,
+  resimler: {
+    bir: String,
+    iki: String,
+    uc: String,
+    dort: String,
+  },
+  stok: {
+    s: Number,
+    m: Number,
+    l: Number,
+    xl: Number,
+  },
+  marka: String,
+  aciklama: String,
+  ind_fiyat: Number,
+  normal_fiyat: Number,
+  yildiz: {
+    sayi: Number,
+    puan: String,
+  },
+  renk: String,
+  parametreler: {
+    taksit: String,
+    garanti: String,
+    kargo: String,
+  },
+  satilma: Number,
+  cinsiyet: String,
+};
 
-app.listen(5000,()=>{
+/////////////////MONGODB MODELLERI//////
 
-    console.log("Server calisiyor...");
-})
+const Urun = mongoose.model("Urun", urunSema);
+
+/////////////////URUN/////////////////
+app.post("/api/urun/olusturma", function (req, res) {
+  var urun = new Urun({
+    isim: "Süvari Gömlek",
+    kategori: "Gömlek",
+    kategori_url: "gomlek",
+    resimler: {
+      bir: "/images/items/1.jpg",
+      iki: "/images/items/2.jpg",
+      uc: "/images/items/3.jpg",
+      dort: "/images/items/4.jpg",
+    },
+    stok: {
+      s: 9,
+      m: 9,
+      l: 9,
+      xl: 9,
+    },
+    marka: "Süvari",
+    aciklama:
+      "Virgil Abloh’s Off-White is a streetwear-inspired collection that continues to break away from the conventions of mainstream fashion. Made in Italy, these black and brown Odsy-1000 low-top sneakers.",
+    ind_fiyat: 29.99,
+    normal_fiyat: 39.99,
+    yildiz: {
+      sayi: 20,
+      puan: "4.4",
+    },
+    renk: "Mavi",
+    parametreler: {
+      taksit: "12 Taksit",
+      garanti: "1 Yıl",
+      kargo: "Ücretsiz",
+    },
+    satilma: 50,
+    cinsiyet: "Erkek",
+  });
+
+  urun.save(function (err) {
+    if (!err) {
+      res.send([
+        {
+          sonuc: "başarılı",
+        },
+      ]);
+    } else {
+      res.send([
+        {
+          sonuc: "hata",
+        },
+      ]);
+    }
+  });
+});
+
+//////////////// POPULER URUNLER //////////////////////
+
+app.get("/api/populerurunler", function (req, res) {
+  Urun.find({}, function (err, gelenVeri) {
+    if (!err) {
+      res.send(gelenVeri);
+    } else {
+      res.send([
+        {
+          sonuc: "hata",
+        },
+      ]);
+    }
+  })
+    .sort({ satilma: -1 })
+    .limit(4);
+});
+
+app.listen(5000, () => {
+  console.log("Server calisiyor...");
+});
